fedd680567461d0ecee0ac622c1989b6e3caeaa6
Eugene Brodsky <eugene@gomotion.ai>
2022-02-25 02:21:03 -0800

    Fin

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Hi Linqia
+Hi Linqia
+
+
+Getting Started
+===================================
+- python version: 2.7
+- pip install -r requirements.txt
+- python server.py
+===================================
+
+
+
+Assumptions:
+===================================
+- keywords are case-insensitive
+- to reset the keywords, you delete the keywords.json file
+===================================
+
+
+
+API Usage:
+===================================
+- Conforms to problem statement
+===================================
+
+
--- server.py
+++ server.py
@@ -1,31 +1,55 @@
 from flask import Flask, jsonify, request
 import json
+import os
+
+baseKeywords = {"vocab": ["#ad", "#sponsored", "advertisement"]}
+
+dbFilePath = "keywords.json"
+
+if not os.path.exists(dbFilePath):
+    fd = open(dbFilePath, 'w')
+    json_string = json.dumps(baseKeywords)
+    json.dump(json_string, fd)
+    fd.close()
+
+fd = open(dbFilePath, "r")
+json_string = json.load(fd)
+dic = json.loads(json_string)
+keywordSet = set(dic["vocab"])
+fd.close()
 
 app = Flask(__name__)
 
-keywordSet = {"#ad", "#sponsered", "advertisement"}
-
 @app.route('/api/vocab', methods=['GET'])
 def getVocab():
-    res = dict.fromkeys(keywordSet, 0)
+    res = {}
+    res["vocab"] = list(keywordSet)
     return res
 
 @app.route('/api/vocab', methods=['POST'])
 def addVocab():
-    keywordList = request.get_json()["vocab"]
-    for x in keywordList:
-        keywordSet.add(x)
-    res = dict.fromkeys(keywordSet,0)
+    newKeywordList = request.get_json()["vocab"]
+    if len(newKeywordList) > 0:
+        for x in newKeywordList:
+            keywordSet.add(x)
+        fd = open(dbFilePath, "w")
+        dic = {"vocab": list(keywordSet)}
+        json_string = json.dumps(dic)
+        json.dump(json_string, fd)
+        fd.close()
+    res = {}
+    res["vocab"] = list(keywordSet)
     return res
 
 @app.route('/api/prediction', methods=['POST'])
 def predict():
-    postText = request.get_json()["post_text"]
-    postWordsSet = set(postText.split(" "))
-    dict = {"prediction": "non-sponsored"}
-    if keywordSet.intersection(postWordsSet):
-        dict["prediction"] = "sponsored"
-    return dict
+    postText = request.get_json()["post_text"].lower()
+    res = {"prediction": "non-sponsored"}
+    for x in keywordSet:
+        if x.lower() in postText:
+            res["prediction"] = "sponsored"
+            break
+    return res
 
 
 if __name__ == '__main__':
